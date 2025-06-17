f95ee72e827c933999b455d66ecf123ac90176b7
HîTESH SîNHA <codonstream.72@gmail.com>
2025-06-17 20:46:55 +0530

    Add files via upload

---
+++ Book_Library_2.py
@@ -0,0 +1,49 @@
+class Book:
+    def __init__(self,name,author):
+        self.name = name
+        self.author = author
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self,book):
+        self.books.append(book)
+        print(f"{book.name} was added")
+
+    def show(self):
+        if self.books == []:
+            print("no books in lib")
+        else:
+            n = 1
+            for x in self.books:
+                print(f"book no.{n} is: {x.name}, author: {x.author}")
+                n+=1
+
+    def take_book(self,name):
+        if self.books == []:
+            return "no books in lib"
+        else:
+            for x in self.books:
+                if name == x.name:
+                    self.books.remove(x)
+                    return self.show()
+                else:
+                    pass
+            return "book not in library"    
+
+    
+lib = Library()
+book1 = Book("book_1","x")
+book2 = Book("book_2","y")
+book3 = Book("book_3","z")
+book4 = Book("book_4","a")
+book5 = Book("book_5","b")
+lib.add_book(book1)
+lib.add_book(book2)
+lib.add_book(book3)
+lib.add_book(book4)
+lib.add_book(book5)
+lib.take_book("book_10")
+
